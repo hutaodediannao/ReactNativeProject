892f4e2331096af8adb96559f1693bd75c243be0
hutao <643759269@qq.com>
2019-07-26 00:41:26 +0800

    1、自定义CardView提交

--- app/Main.js
+++ app/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import {View, Text, Image, ListView, ScrollView, ToastAndroid} from 'react-native';
+import {View, Text, Image, ListView, ScrollView, ToastAndroid,StyleSheet} from 'react-native';
 import HeaderView from './component/HeaderView.js';
 import TabLayout from './component/TabLayout';
 import BannerLayout from './component/BannerView.js';
+import CardView from './component/CardView';
 
 const data = ['我的最爱', '热门内容', '优美话题'];
 
@@ -12,9 +13,11 @@ class MainPage extends React.Component {
         return (
             <View>
                 <HeaderView leftClick={this.left} centerTitle="这是主页" mode={2} clickRight={this.clickRight}/>
-                <TabLayout datas={data}/>
                 <BannerLayout/>
-
+                <TabLayout datas={data}/>
+                <View style={styles.bodyContainerStyle}>
+                <CardView/>
+                </View>
             </View>
         );
     }
@@ -27,5 +30,14 @@ class MainPage extends React.Component {
         ToastAndroid.show('right', ToastAndroid.SHORT);
     };
 }
+
+const styles = StyleSheet.create({
+    bodyContainerStyle:{
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: 10,
+        marginBottom: 10,
+    }
+});
 
 export default MainPage;
---
+++ app/component/CardView.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet,} from 'react-native';
+
+class CardView extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <View style={styles.cardViewStyle}>
+
+                <Image style={styles.image} source={{uri: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564082493599&di=9cd26582572673a41b378fa6c7b41601&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fphotoblog%2F1401%2F31%2Fc8%2F30970720_1391180640512.jpg'}}/>
+                <Text style={styles.text}>这是我的自定义文字</Text>
+
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    cardViewStyle:{
+        width: 200,
+        height: 120,
+        borderRadius: 10,
+        backgroundColor: 'red',
+    },
+    image:{
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+    },
+    text: {
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        color: 'white',
+        fontSize: 17,
+        alignItems: 'center',
+        backgroundColor: 'red',
+        textAlign: 'center',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+});
+
+export default CardView;
